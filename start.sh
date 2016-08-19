cd6aeeee8937d935d061f91f60324e4db9ebc6fc
Đorđe Cvijanović <cdorde@gmail.com>
2016-08-19 14:54:23 +0200

    Removed 4.5.0 tag so command will use latest

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ $# -eq 4 ]; then
     if [ $4 = "F" ]; then
-       docker run --name $1 -i -t -p $2:8080 -v $3:/usr/local/tomcat/webapps/birt/reports cdorde/mybirt:4.5.0
+       docker run --name $1 -i -t -p $2:8080 -v $3:/usr/local/tomcat/webapps/birt/reports cdorde/mybirt
     else
        if [ $4 = "D" ]; then
-          docker run --name $1 -d --restart=always -i -t -p $2:8080 -v $3:/usr/local/tomcat/webapps/birt/reports cdorde/mybirt:4.5.0
+          docker run --name $1 -d --restart=always -i -t -p $2:8080 -v $3:/usr/local/tomcat/webapps/birt/reports cdorde/mybirt
        else
           echo "wrong syntax. You must provide four parameters"
           echo "sh start.sh <Name of container> <Port where tomcat will server Birt> <Path to reports directory> <(F)oreground or (D)aemon mode"
